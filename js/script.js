b358d658a697e6ad176e7e8362e7d2c911e0c95b
mattJamesMedia <mattjamesmedia@gmail.com>
2017-03-03 19:14:41 -0600

    adding alternate header and of canvas nav

--- js/script.js
+++ js/script.js
@@ -8,7 +8,14 @@ function appendSvg(){
     document.body.insertBefore(div, document.body.childNodes[0]);
   }
 } 
-
+function toggleNav(){
+  var navTrigger = document.querySelector('.header__navTrigger');
+  var nav = document.querySelector('.nav');
+  navTrigger.classList.toggle('header__navTrigger--clicked');
+  nav.classList.toggle('nav--open');
+  navTrigger.classList.contains('header__navTrigger--clicked') ? navTrigger.setAttribute('href', "#") : navTrigger.setAttribute('href', "#nav");
+}
+document.querySelector('.header__navTrigger').addEventListener('click', toggleNav);
 document.addEventListener('DOMContentLoaded', function(){
   appendSvg();
 });
